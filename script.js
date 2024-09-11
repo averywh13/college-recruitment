b4bfc04549c1ea8b03e4a529c0fe4301d85676ff
averywh13 <awekslerhart@gmail.com>
2024-09-11 13:47:36 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,29 @@
+function information() { 
+    document.getElementById('changeImage').src = 'imgs/christmastree.png'
+    document.getElementById('response-text').innerText='Christmas tree, Oh Christmas tree! The first Christmas tree to be lit by electric lights was reportedly in the home of Edward H. Johnson, an associate of inventor Thomas Edison. This historic event took place on December 22, 1882, in New York City. Edward Johnson, who was an Edison employee, hand-wired 80 red, white, and blue electric light bulbs and decorated the Christmas tree in his home with them. This marked one of the early uses of electric lights for holiday decorations and contributed to the popularization of this practice in the years that followed. ' 
+      document.getElementById('response-date').innerText= 'December 22, 1882' 
+  } 
+  
+  function programs() {
+    document.getElementById('changeImage').src = 'imgs/asteroid.png'
+    document.getElementById('response-text').innerText='Quick! Make a wish! The first asteroid to be discovered using photography was 323 Brucia. It was discovered by the American astronomer Max Wolf on December 22, 1891, at the Heidelberg Observatory in Germany. Max Wolf pioneered the use of astrophotography, a technique that involves taking photographs of the night sky, to detect and study asteroids and other celestial objects. The discovery of 323 Brucia marked a significant advancement in observational astronomy during the late 19th century'
+      document.getElementById('response-date').innerText= 'December 22, 1891'
+  } 
+  
+  function life() {
+    document.getElementById('changeImage').src = 'imgs/johnwaynegacy.webp'
+    document.getElementById('response-text').innerText='John Wayne Gacy confesses to police to killing over two dozen boys and young men and burying their bodies under his suburban Chicago home. In March 1980, Gacy was convicted of 33 sex-related murders, committed between 1972 and 1978, and later given the death penalty. '
+      document.getElementById('response-date').innerText= 'December 22, 1978'
+  }
+  
+  function cost() {
+    document.getElementById('changeImage').src = 'imgs/richardcreid.jpg'
+    document.getElementById('response-text').innerText='Richard C. Reid boarded American Airlines Flight 63 from Paris to Miami with homemade bombs hidden in his shoes. During the flight, Reid tried to detonate his shoes, but struggled to light the fuse. Crew members and passengers noticed and restrained him. FBI agents determined that the shoes contained about ten ounces of explosive material. During a preliminary hearing. and FBI agent reveals how dangerous the bomb was. She said that the bomb techs determined that the bomb would have blown a hole in the planes fuselage and caused the plant to crash if it has detonated. '
+      document.getElementById('response-date').innerText= 'December 22, 2001' 
+  }
+  
+  function alumni() {
+    document.getElementById('changeImage').src = 'imgs/baby2.png'
+    document.getElementById('response-text').innerText='December 22, 2007 11:26am, me! I was born! The blue-eyed, ginger-haired girl, that was born from two parents with dark hair and dark eyes. I question it too, trust me. '
+      document.getElementById('response-date').innerText= 'December 22, 2007'
+  }
