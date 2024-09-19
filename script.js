1e58a650a12983b74592d979ef88aa57ffed97ae
averywh13 <awekslerhart@gmail.com>
2024-09-19 18:30:58 -0400

    chapter 6 - finished js - need to add details

--- script.js
+++ script.js
@@ -13,53 +13,53 @@ function information() {
 
 
   function programs() {
-    document.getElementById('changeImage1').src = 'imgs/...'
-    document.getElementById('response-text1').innerText='....'
+    document.getElementById('changeImage1').src = 'imgs/lehighprograms1.jpeg'
+    document.getElementById('response-text1').innerText='Lehigh University offers a wide variety of majors across if fours colleges. The P.C. Rossin College of Engineering and Applied Science which is known for mechanical, civil, electrical engineering, as well as bioengineering and computer science majors. The College of Business is known for its finance, marketing, accounting, economics, supply chain management, and business analytics majors. The College of Arts and Sciences is known for its psychology, political science, biology, english, international relations, and environmental science majors. The College of Education is known for its counseling, educational leadership. instructional technology, and school psychology majors. Lastly, the Interdisciplinary programs are an integrated degrees in engineering, arts, and sciences, as well as cognitive science and environmental studies majors.'
 
-    document.getElementById('changeImage2').src = 'imgs/...'
-    document.getElementById('response-text2').innerText='....'
+    document.getElementById('changeImage2').src = 'imgs/lehighprograms2.jpeg'
+    document.getElementById('response-text2').innerText='Lehigh’s curriculum is designed to be flexible, encouraging interdisciplinary learning. Each program typically includes core courses, major-specific requirements, electives, and optional minors. For example, engineering students take foundational courses in mathematics, physics, and engineering principles, followed by specialized courses in their chosen field. Business students start with core subjects like economics and management before diving into advanced finance or marketing courses. Many programs also offer opportunities for internships, co-ops, and research projects to apply classroom knowledge.'
 
-    document.getElementById('changeImage3').src = 'imgs/...'
-    document.getElementById('response-text3').innerText='....'
+    document.getElementById('changeImage3').src = 'imgs/lehighprograms3.jpeg'
+    document.getElementById('response-text3').innerText='Lehigh University’s faculty consists of accomplished scholars and researchers. In engineering, faculty members are experts in areas like robotics, energy systems, and nanotechnology. The College of Business boasts professionals with extensive knowledge in entrepreneurship, global finance, and strategic management. Faculty in the College of Arts and Sciences include renowned historians, literary scholars, and scientists working in fields like neuroscience and environmental conservation. Faculty members actively involve students in research projects and provide mentorship to support both academic and career growth.'
   }
 
 
 
   function life() {
-    document.getElementById('changeImage1').src = 'imgs/...'
-    document.getElementById('response-text1').innerText='.....'
+    document.getElementById('changeImage1').src = 'imgs/lehighlife1.jpeg'
+    document.getElementById('response-text1').innerText='Lehigh University offers a modern campus with state-of-the-art facilities, including research labs, libraries, fitness centers, and athletic spaces like the Stabler Arena. Students have access to dining options, wellness services, and innovative spaces such as the Wilbur Powerhouse, which supports entrepreneurship. The campus’s blend of historical and contemporary architecture complements its scenic surroundings, providing an inspiring environment for study and recreation.'
 
-    document.getElementById('changeImage2').src = 'imgs/...'
-    document.getElementById('response-text2').innerText='.....'
+    document.getElementById('changeImage2').src = 'imgs/lehighlife2.jpeg'
+    document.getElementById('response-text2').innerText='With over 200 student organizations, Lehigh fosters an active student life. Clubs range from academic and professional groups to recreational and cultural organizations, offering leadership, service, and social opportunities. Greek life is prominent, with numerous fraternities and sororities. Lehigh also hosts a range of extracurricular activities, including concerts, theater productions, intramural sports, and campus traditions like the historic Lehigh-Lafayette football rivalry.'
 
-    document.getElementById('changeImage3').src = 'imgs/...'
-    document.getElementById('response-text3').innerText='.....'
+    document.getElementById('changeImage3').src = 'imgs/lehighlife3.webp'
+    document.getElementById('response-text3').innerText='Housing at Lehigh includes a variety of on-campus options, such as traditional residence halls, suite-style living, and themed communities. Freshmen are typically required to live on campus, and upperclassmen have access to apartment-style housing. Housing costs vary, with prices ranging from around $7,000 to $10,000 annually, depending on the accommodation and meal plan.'
   }
   
 
 
   function cost() {
-    document.getElementById('changeImage1').src = 'imgs/...'
-    document.getElementById('response-text1').innerText='......'
+    document.getElementById('changeImage1').src = 'imgs/lehighcost1.png'
+    document.getElementById('response-text1').innerText='Lehigh University’s tuition for undergraduate students is approximately $60,000 per year, with additional costs for housing, meals, and other fees bringing the total estimated annual cost to around $75,000. Graduate program costs vary by school and specific degree, with per-credit tuition rates for part-time programs. Fees include student activity fees, technology fees, and health insurance, though students can waive the latter if they have their own coverage.'
 
-    document.getElementById('changeImage2').src = 'imgs/...'
-    document.getElementById('response-text2').innerText='......'
+    document.getElementById('changeImage2').src = 'imgs/lehighcost2.png'
+    document.getElementById('response-text2').innerText='Lehigh offers a range of financial aid options, including need-based aid, scholarships, grants, and student loans. The university is committed to meeting 100% of demonstrated financial need for eligible students through a combination of grants, work-study, and federal loans. Merit-based scholarships are also available, particularly for students who excel academically or demonstrate leadership and community involvement. Other resources include specialized grants for specific programs or groups, such as the Lehigh Legacy Scholarship.'
 
-    document.getElementById('changeImage3').src = 'imgs/...'
-    document.getElementById('response-text3').innerText='......'
+    document.getElementById('changeImage3').src = 'imgs/lehighcost3.jpeg'
+    document.getElementById('response-text3').innerText='Lehigh provides flexible payment options, including semester-based installment payment plans to help families spread the cost over time. Payments can be made monthly, with deadlines typically at the start of each semester. The university also offers guidance on FAFSA and CSS Profile applications, which are critical for financial aid consideration. Deadlines for financial aid applications typically fall in early spring for the following academic year, while tuition payment deadlines vary depending on the chosen plan.'
   }
   
 
 
   function alumni() {
-    document.getElementById('changeImage1').src = 'imgs/..'
-    document.getElementById('response-text1').innerText='.......'
+    document.getElementById('changeImage1').src = 'imgs/lehighalumni1.jpeg'
+    document.getElementById('response-text1').innerText='Lido Anthony "Lee" Iacocca, studied industrial engineering, was an American automobile executive best known for the development of the Ford Mustang, Continental Mark III, and Ford Pinto cars while at the Ford Motor Company in the 1960s, and for reviving the Chrysler Corporation as its CEO during the 1980s. He was president of Chrysler from 1978 to 1991 and chairman and CEO from 1979 until his retirement at the end of 1992. He was one of the few executives to preside over the operations of two of the United States’ Big Three automakers.'
 
-    document.getElementById('changeImage2').src = 'imgs/...'
-    document.getElementById('response-text2').innerText='.......'
+    document.getElementById('changeImage2').src = 'imgs/lehighalumni2.jpeg'
+    document.getElementById('response-text2').innerText='Frederick Crist Trump Jr., graduated with Bachelor of Arts in busisness was an American airplane pilot and maintenance worker. The eldest son of real-estate businessman Fred Trump Sr., he fell out of his father’s favor when he chose to become an airline pilot, leading to his younger brother Donald (the 45th president of the United States) inheriting the family business.'
 
-    document.getElementById('changeImage3').src = 'imgs/...'
-    document.getElementById('response-text3').innerText='.......'
+    document.getElementById('changeImage3').src = 'imgs/lehighalumni3.jpeg'
+    document.getElementById('response-text3').innerText='Terry Jonathan "T. J." Hart is an American mechanical and electrical engineer, a retired United States Air Force lieutenant colonel and pilot, and former NASA astronaut. Hart served as a mission specialist on the STS-41-C mission, where tasks included operation of the shuttle Remote Manipulator System (RMS) arm to deploy the Long Duration Exposure Facility (LDEF) and Solar Max satellite.'
   }
   // MY JAVA
 
